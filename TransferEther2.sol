9eee8384bfe2fa553c3aad41a4c3f5222894d603
Anupam Vaishy <Anupam.Vaishy@reveation.io>
2022-07-04 12:31:40 +0530

    commit after formatting

--- TransferEther2.sol
+++ TransferEther2.sol
@@ -5,30 +5,27 @@
   which send the Ether from one address to another */
 pragma solidity ^0.8.4;
 contract task{
-    address public manager;
-    address  payable public  recipient;
-    address  payable public  per;
+    address public Deployer;//This datatype is use for storing the address of deployer
+    address payable public recipient;//This  datatype is use for recieve payment.
+    address payable public setAddressOfReciever;//This datatype is use for setting address of recipient.
 
-    constructor()
+ constructor()//this constructor is used for restore the address of deployer
     {
-        manager=msg.sender;
+        Deployer=msg.sender;
     }
   
- function getter() public view returns(address payable)
+ function getter() public view returns(address payable)//This getter function is used for return the address of reciever
     {
-return  per;
+        return  setAddressOfReciever;
     }
-     function setter(address payable recipient)  payable external
+function setter(address payable recipient)  payable external// This setter function is used for set the address of reciever.
     {
-      per=recipient;
-  }
+        setAddressOfReciever=recipient;//In this line Address of recipient stored in setAddressOfReciever
+    }
 
-    function sent(address payable  recipient) payable external{
-          require(msg.value >= 1);
-        recipient.transfer(msg.value);
+function sent(address payable  recipient) payable external//This function is used for sent payment from one account to another
+    {
+        require(msg.value >= 1);//This line ensure that there will be tranfer ether is equals or more that one with the help of require function.
+        recipient.transfer(msg.value);//This line give address of recipient in which we want to sent payment.  
    }
-
-
-    
-
 }
